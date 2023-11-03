99858b3e647e620b20cc36e1df4675558fbf28ef
Shravya.Shetty <125678653+shrvya-shetty@users.noreply.github.com>
2023-11-03 17:30:21 +0530

    Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -27,15 +27,42 @@ public class Main {
     return counts;
   }
 
-  static void testBucketingByHealth() {
+ static void testBucketingByHealth() {
     System.out.println("Counting batteries by SoH...\n");
+
+    // Original test case
     int[] presentCapacities = {113, 116, 80, 95, 92, 70};
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
+
+    // Additional test cases
+    int[] presentCapacities1 = {113, 116, 80, 95, 92, 70, 105, 110};
+    CountsBySoH counts1 = countBatteriesByHealth(presentCapacities1);
+    assert(counts1.healthy == 4);
+    
+    int[] presentCapacities2 = {75, 78, 60, 62, 77, 81};
+    CountsBySoH counts2 = countBatteriesByHealth(presentCapacities2);
+    assert(counts2.exchange == 4);
+    
+    int[] presentCapacities3 = {115, 118, 120, 120, 120, 120};
+    CountsBySoH counts3 = countBatteriesByHealth(presentCapacities3);
+    assert(counts3.healthy == 6);
+    
+    int[] presentCapacities4 = {50, 55, 40, 59, 48};
+    CountsBySoH counts4 = countBatteriesByHealth(presentCapacities4);
+    assert(counts4.failed == 5);
+    
+    int[] presentCapacities5 = {};
+    CountsBySoH counts5 = countBatteriesByHealth(presentCapacities5);
+    assert(counts5.healthy == 0);
+    assert(counts5.exchange == 0);
+    assert(counts5.failed == 0);
+    
     System.out.println("Done counting :)\n");
-  }
+}
+
 
   public static void main(String[] args) {
     testBucketingByHealth();
