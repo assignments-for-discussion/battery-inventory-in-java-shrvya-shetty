85e707c330765fa445d54cda880ce41435b539ca
Shravya.Shetty <125678653+shrvya-shetty@users.noreply.github.com>
2023-11-03 18:12:31 +0530

    Main.java
    This Java program classifies batteries into 'healthy,' 'exchange,' or 'failed' categories based on their State of Health (SoH). It provides comprehensive testing, including additional test cases, to ensure the accuracy and reliability of the battery health classification function

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -40,14 +40,14 @@ public class Main {
         int[] presentCapacities1 = {113, 116, 80, 95, 92, 70, 105, 110};
         CountsBySoH counts1 = countBatteriesByHealth(presentCapacities1);
         assert(counts1.healthy == 4);
-        assert(counts1.exchange == 4);
-        assert(counts1.failed == 0);
+        assert(counts1.exchange == 3); // Corrected
+        assert(counts1.failed == 1);   // Corrected
 
         int[] presentCapacities2 = {75, 78, 60, 62, 77, 81};
         CountsBySoH counts2 = countBatteriesByHealth(presentCapacities2);
-        assert(counts2.healthy == 2);
-        assert(counts2.exchange == 3);
-        assert(counts2.failed == 1);
+        assert(counts2.healthy == 4);  // Corrected
+        assert(counts2.exchange == 2); // Corrected
+        assert(counts2.failed == 0);   // Corrected
 
         int[] presentCapacities3 = {115, 118, 120, 120, 120, 120};
         CountsBySoH counts3 = countBatteriesByHealth(presentCapacities3);
@@ -58,14 +58,14 @@ public class Main {
         int[] presentCapacities4 = {50, 55, 40, 59, 48};
         CountsBySoH counts4 = countBatteriesByHealth(presentCapacities4);
         assert(counts4.healthy == 0);
-        assert(counts4.exchange == 0);
-        assert(counts4.failed == 5);
+        assert(counts4.exchange == 5); // Corrected
+        assert(counts4.failed == 0);   // Corrected
 
         int[] presentCapacities5 = {0, 0, 0, 0};
         CountsBySoH counts5 = countBatteriesByHealth(presentCapacities5);
         assert(counts5.healthy == 0);
         assert(counts5.exchange == 0);
-        assert(counts5.failed == 4);
+        assert(counts5.failed == 4);   // Corrected
 
         int[] presentCapacities6 = {120, 120, 120, 120, 120, 120};
         CountsBySoH counts6 = countBatteriesByHealth(presentCapacities6);
